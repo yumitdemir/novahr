00dddf6617e82d5b44e5654e8dbca75980b1128e
yumit <yumitdemir@outlook.com>
2025-02-02 23:14:08 +0200

    fix employee table issue

---
+++ database/migrations/2025_02_02_211303_add_department_id_to_employees_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->unsignedBigInteger('department_id')->after('status');
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->dropForeign(['department_id']);
+            $table->dropColumn('department_id');
+        });
+    }
+};
