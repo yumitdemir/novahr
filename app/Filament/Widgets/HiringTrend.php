4de62b00c5a5bcc1894962790a0033117a283131
yumit <yumitdemir@outlook.com>
2025-02-08 19:36:51 +0200

    add first chart hiring trend

---
+++ app/Filament/Widgets/HiringTrend.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Employee;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class HiringTrend extends ChartWidget
+{
+    protected static ?string $heading = 'Chart';
+    protected static string $color = 'info';
+    protected function getData(): array
+    {
+        $data = Trend::model(Employee::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Employee Hired This Year',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
